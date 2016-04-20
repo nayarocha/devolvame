1068d9a76663f6cf882160900285d7068b9b557c
Ubuntu <nayaarocha@gmail.com>
2016-04-19 20:47:04 -0700

    2. Criar projeto de desenvolvimento com start.spring.io

---
+++ src/main/java/br/edu/ifrn/DevolvameApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifrn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevolvameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevolvameApplication.class, args);
+	}
+}
