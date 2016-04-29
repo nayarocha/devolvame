555b7afdc7eb5655e175aa5dfc8b21b02dc6cc84
Fabinho do FBI <Fabinho do FBI@Fabinho>
2016-04-29 07:58:04 -0300

    fix #5

--- src/main/java/br/edu/ifrn/DevolvameApplication.java
+++ src/main/java/br/edu/ifrn/DevolvameApplication.java
@@ -1,45 +1,12 @@
 package br.edu.ifrn;
 
-import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.ConfigurableWebApplicationContext;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
-public class DevolvameApplication implements ApplicationContextAware {
-        private ApplicationContext context;
-	
-        public static void main(String[] args) {
+public class DevolvameApplication {
+
+	public static void main(String[] args) {
 		SpringApplication.run(DevolvameApplication.class, args);
-                
-        }
-                @RequestMapping("/sair")
-                public void closeApp(){
-                    password("devolva");
-                }
-
-                @Override
-                public void setApplicationContext(ApplicationContext ac) throws BeansException {
-                    context = ac;
-                }
-                
-                @RequestMapping("/teste")
-                public String execute() {
-                    System.out.println("testando");
-                return "Apenas um teste";
-                }
-                
-                public void password(String string){
-                  if (string == "devolva")
-                      ((ConfigurableWebApplicationContext)this.context).close();
-                  else {
-                      System.out.println("Senha incorreta");
-                  }
-                } 
+	}
 }
-
