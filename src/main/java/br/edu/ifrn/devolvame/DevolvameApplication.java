f1b60b9d8d52c159489a0da7b15e3776e0f5f135
Ubuntu <nayaarocha@gmail.com>
2016-05-18 11:23:12 -0700

    #fix 14

--- src/main/java/br/edu/ifrn/devolvame/DevolvameApplication.java
+++ src/main/java/br/edu/ifrn/devolvame/DevolvameApplication.java
@@ -1,12 +1,17 @@
 package br.edu.ifrn.devolvame;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DevolvameApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevolvameApplication.class, args);
+              new SpringApplicationBuilder()                
+                .sources(DevolvameApplication.class)
+                .run(args);
+		
 	}
 }
