c87671fbb505f173b9d19a0d9fd36cd428b3d33e
Pedro Rafael <pedrorafael0251@gmail.com>
2016-05-30 19:58:10 -0300

    #16 - Método CompareTo - Classe Livro

--- src/test/java/br/edu/ifrn/devolvame/dominio/LivroTests.java
+++ src/test/java/br/edu/ifrn/devolvame/dominio/LivroTests.java
@@ -1,4 +1,6 @@
 package br.edu.ifrn.devolvame.dominio;
+import java.util.Set;
+import java.util.TreeSet;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.testng.annotations.Test;
 
@@ -22,5 +24,15 @@ public class LivroTests {
     public void tituloDiferenteComEditoraIgual(){
         assertThat(Livro.builder().titulo(titulo).editora(editora).build())
                 .isNotEqualTo(Livro.builder().titulo(outro_titulo).editora(editora).build());
+    }
+       
+    public void CompareTo(){
+        Set<Livro> livros = new TreeSet<>();
+        Livro livro2 = Livro.builder().titulo(titulo).build();
+        livros.add(livro2);
+        Livro livro1 = Livro.builder().titulo(outro_titulo).build();
+        livros.add(livro1);
+        
+        assertThat(livros.iterator().next()).isEqualTo(livro2);
     }
 }
