f34ec203735686475012762b33427b0f548c9fdb
Pedro Rafael <pedrorafael0251@gmail.com>
2016-05-30 20:18:57 -0300

    #23 - Persistência das Classes - Livro e Usuário

--- src/main/java/br/edu/ifrn/devolvame/dominio/Livro.java
+++ src/main/java/br/edu/ifrn/devolvame/dominio/Livro.java
@@ -1,5 +1,13 @@
 package br.edu.ifrn.devolvame.dominio;
 import java.awt.image.BufferedImage;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -16,18 +24,30 @@ import lombok.ToString;
 @ToString
 @EqualsAndHashCode(of = {"titulo"})
 @Builder
-public class Livro implements Comparable<Livro>{
+@Entity
+@SequenceGenerator(sequenceName = "seq_usuario", name = "ID_SEQUENCE", allocationSize = 1)
+public class Livro implements Serializable, Comparable<Livro>{
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "ID_SEQUENCE")
+    private Long id;
+    
     private int idLivro;
+    @Column(nullable = false)
     private String titulo;
+    @Column(nullable = false)
     private int isbn;
     private String editora;
     private String idioma;
     private String descricao;
     private BufferedImage img;
     private int quant;
+    @OneToOne(mappedBy = "livro")
     private Categoria categoria;
     private Autor autor;
     //Status do livro - emprestado/disponivel
+    @Column(nullable = false)
     private int status;
 
     @Override
--- src/main/java/br/edu/ifrn/devolvame/dominio/Usuario.java
+++ src/main/java/br/edu/ifrn/devolvame/dominio/Usuario.java
@@ -1,6 +1,15 @@
 package br.edu.ifrn.devolvame.dominio;
 import java.awt.image.BufferedImage;
+import java.io.Serializable;
 import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -16,15 +25,27 @@ import lombok.ToString;
 @ToString
 @EqualsAndHashCode(of = {"email"})
 @Builder
-public class Usuario implements /*Serializable,*/ Comparable<Usuario>{
+@Entity
+@SequenceGenerator(sequenceName = "seq_usuario", name = "ID_SEQUENCE", allocationSize = 1)
+public class Usuario implements Serializable, Comparable<Usuario>{
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "ID_SEQUENCE")
+    private Long id;
+    
     private int idUsuario;
+    @Column(nullable = false)
     private String nome;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private String senha;
     private int telefone;
     private BufferedImage img;
+    @OneToOne(mappedBy = "usuario")
     private Acervo acervo; 
-    
+    @OneToMany(mappedBy = "usuario")
     private Set<Emprestimo> listagemEmprestimo; 
     
     @Override
