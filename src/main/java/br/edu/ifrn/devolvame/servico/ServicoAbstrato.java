f5e3053dcea1c0e7f8196003f5904a6392cd4fbe
Pedro Rafael <pedrorafael0251@gmail.com>
2016-06-02 09:46:36 -0300

    fix #33

---
+++ src/main/java/br/edu/ifrn/devolvame/servico/ServicoAbstrato.java
@@ -0,0 +1,43 @@
+package br.edu.ifrn.devolvame.servico;
+
+import br.edu.ifrn.devolvame.persistencia.Repositorio;
+import java.io.Serializable;
+import java.util.Iterator;
+import javax.inject.Inject;
+
+public class ServicoAbstrato<ID extends Serializable, O, R extends Repositorio<ID, O>> implements Servico<ID, O>{
+    
+    private R repositorio;
+    
+    @Inject
+    public ServicoAbstrato(R repositorio){
+        this.repositorio = repositorio;
+    }
+    
+    @Override
+    public void create(ID id, O object) {
+        repositorio.create(id, object);
+    }
+
+    @Override
+    public O retrieve(ID id) {
+        return repositorio.retrieve(id);
+    }
+
+    @Override
+    public void update(ID id, O object) {
+        repositorio.update(id, object);
+    }
+
+    @Override
+    public void delete(ID id) {
+        repositorio.delete(id);
+    }
+
+    @Override
+    public Iterator<O> iterator() {
+        return null;
+    }
+   
+    
+}
