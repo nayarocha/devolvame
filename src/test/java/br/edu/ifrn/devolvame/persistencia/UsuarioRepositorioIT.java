125cce42576aea2df85881c204b512b1065f1af5
Nayara Rocha <nayaarocha@gmail.com>
2016-06-02 22:31:05 -0700

    fix #31

---
+++ src/test/java/br/edu/ifrn/devolvame/persistencia/UsuarioRepositorioIT.java
@@ -0,0 +1,40 @@
+package br.edu.ifrn.devolvame.persistencia;
+
+import br.edu.ifrn.devolvame.DevolvameApplication;
+import br.edu.ifrn.devolvame.dominio.Usuario;
+import javax.inject.Inject;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.testng.annotations.Test;
+
+@SpringApplicationConfiguration(classes = DevolvameApplication.class)
+@WebAppConfiguration
+@Test
+public class UsuarioRepositorioIT extends AbstractTestNGSpringContextTests{
+    @Inject 
+    private UsuarioRepositorio usuarioRepositorio;
+    
+    
+    public void isNotNull () {
+        assertThat(usuarioRepositorio).isNotNull();
+    }
+    
+    public void save(){
+        Usuario usuario = Usuario.builder().nome("User01").build();
+        usuarioRepositorio.save(usuario);
+        
+        assertThat(usuarioRepositorio.iterator().hasNext()).isEqualTo(usuario);
+    }
+    
+    
+    public void delete(){
+        Usuario usuario = Usuario.builder().nome("User01").build();
+        usuarioRepositorio.save(usuario);
+        
+        usuarioRepositorio.delete(usuario);
+        
+        assertThat(usuarioRepositorio.iterator().hasNext()).isFalse();
+    }
+}
