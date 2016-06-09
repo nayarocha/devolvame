cbfd955c775113f54cda8381a7327602b73f383e
Adolfo Melo <adf.melo@gmail.com>
2016-06-09 16:58:51 -0300

    testa compareTo do modelo Autor

--- src/test/java/br/edu/ifrn/devolvame/dominio/AutorTest.java
+++ src/test/java/br/edu/ifrn/devolvame/dominio/AutorTest.java
@@ -2,6 +2,9 @@ package br.edu.ifrn.devolvame.dominio;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import org.testng.annotations.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  *
@@ -10,16 +13,24 @@ import org.testng.annotations.Test;
 @Test
 public class AutorTest {
     
-    private static final String NOME_DO_AUTOR = "Clarice Lispector";
-    private static final String OUTRO_NOME_DE_AUTOR = "Paulo Freire";
+    private static final String NOME_DA_AUTORA = "Clarice Lispector";
+    private static final String NOME_DE_AUTOR = "Paulo Freire";
     
     public void autoresTemOMesmoNome(){
         
-        assertThat(Autor.builder().autor(NOME_DO_AUTOR).build())
-            .isEqualTo(Autor.builder().autor(NOME_DO_AUTOR).build());
+        assertThat(Autor.builder().autor(NOME_DA_AUTORA).build())
+            .isEqualTo(Autor.builder().autor(NOME_DA_AUTORA).build());
     }
     public void autoresTemNomesDiferentes(){
-        assertThat(Autor.builder().autor(NOME_DO_AUTOR).build())
-            .isNotEqualTo(Autor.builder().autor(OUTRO_NOME_DE_AUTOR).build());
+        assertThat(Autor.builder().autor(NOME_DA_AUTORA).build())
+            .isNotEqualTo(Autor.builder().autor(NOME_DE_AUTOR).build());
+    }
+    
+    public void compareTo(){
+        Autor autor = Autor.builder().autor(NOME_DE_AUTOR).build();
+        Autor autora = Autor.builder().autor(NOME_DA_AUTORA).build();
+        
+        assertThat(autor.compareTo(autora)).isGreaterThan(0);
+        
     }
 }
