07d8f3f40d12df88213d18d6fe4d94cd415fce4a
Adolfo Melo <adf.melo@gmail.com>
2016-06-10 04:05:11 -0300

    fix #44 implementa testes de integração para os Repository Methods da classe Categoria com Query Methods

--- src/test/java/br/edu/ifrn/devolvame/persistencia/CategoriaRepositorioIT.java
+++ src/test/java/br/edu/ifrn/devolvame/persistencia/CategoriaRepositorioIT.java
@@ -7,34 +7,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 @SpringApplicationConfiguration(classes = DevolvameApplication.class)
 @WebAppConfiguration
-@Test
+@Test(groups = "categoria")
 public class CategoriaRepositorioIT extends AbstractTestNGSpringContextTests{
+    
     @Inject 
     private CategoriaRepositorio categoriaRepositorio;
     
+    @Inject
+    private FabricaDominio dominioFactory;
     
+    @BeforeMethod
+    void deletarTodos()
+    {
+        categoriaRepositorio.deleteAll();
+        assertThat(categoriaRepositorio.findAll()).isEmpty();
+    }
+    
+
     public void isNotNull () {
         assertThat(categoriaRepositorio).isNotNull();
     }
     
     public void save(){
         Categoria categoria = Categoria.builder().nomeCategoria("biografia").build();
+        
+        // executa a operacao a ser testada
         categoriaRepositorio.save(categoria);
         
-        assertThat(categoriaRepositorio.iterator().hasNext()).isEqualTo(categoria);
+        assertThat(categoriaRepositorio.findAll().iterator().next()).isEqualTo(categoria);
     }
     
     
     public void delete(){
-        Categoria categoria = Categoria.builder().nomeCategoria("biografia").build();
-        categoriaRepositorio.save(categoria);
+        Categoria categoria = dominioFactory.ficcao();
         
+        // executa a operacao a ser testada
         categoriaRepositorio.delete(categoria);
         
-        assertThat(categoriaRepositorio.iterator().hasNext()).isFalse();
+        assertThat(categoriaRepositorio.findOne(categoria.getId())).isNull();
     }
 }
