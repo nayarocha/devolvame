d76bd349a806cf5fb3ba846b1798b7a5db39fc3e
Pedro Rafael <pedrorafael0251@gmail.com>
2016-08-19 08:51:15 -0300

    Conexão com Facebook - refatorado

--- src/main/java/br/edu/ifrn/devolvame/HelloController.java
+++ src/main/java/br/edu/ifrn/devolvame/HelloController.java
@@ -1,6 +1,9 @@
 package br.edu.ifrn.devolvame;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.facebook.api.Facebook;
@@ -15,22 +18,27 @@ public class HelloController {
 
     private Facebook facebook;
 	private ConnectionRepository connectionRepository;
-
+       
     @Inject
     public HelloController(Facebook facebook, ConnectionRepository connectionRepository) {
         this.facebook = facebook;
         this.connectionRepository = connectionRepository;
+    
     }
 
     @RequestMapping(method=RequestMethod.GET)
-    public String helloFacebook(Model model) {
+    public String helloFacebook(Model model, HttpServletRequest request) {
         if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
             return "redirect:/connect/facebook";
         }
 
         model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile()); 
-        model.addAttribute("userID", facebook.userOperations().getUserProfile().getId());
-       
-        return "hello";
+        //model.addAttribute("userID", facebook.userOperations().getUserProfile().getId());
+    
+        HttpSession session = request.getSession();
+        HttpSession nome = request.getSession();
+        session.setAttribute("userID", facebook.userOperations().getUserProfile().getId());
+        nome.setAttribute("nome", facebook.userOperations().getUserProfile().getName());
+        return "redirect:livro.jsf";
     }
 }
